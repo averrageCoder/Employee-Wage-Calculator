e4f5a81ccc30031e4e7c6a81f86b44e07dd42db2
Harsh R Jain <rjainharsh@gmail.com>
2021-08-20 15:58:08 +0530

    ADDED: welcome mssg

---
+++ src/com/bridgelabz/employeewagecalculator/EmployeeWageCalculator.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.employeewagecalculator;
+
+public class EmployeeWageCalculator {
+	
+	public static void main(String[] args) {
+
+		System.out.println("<=== Welcome to Employee Wage Computation Program ===>");
+	
+	}
+
+}
