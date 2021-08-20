b5e3f4eeb5d756b8fe54711a99a78cf7d400abfc
Harsh R Jain <rjainharsh@gmail.com>
2021-08-20 16:08:33 +0530

    ADDED: absent/present check

--- src/com/bridgelabz/employeewagecalculator/EmployeeWageCalculator.java
+++ src/com/bridgelabz/employeewagecalculator/EmployeeWageCalculator.java
@@ -4,7 +4,14 @@ public class EmployeeWageCalculator {
 	
 	public static void main(String[] args) {
 
-		System.out.println("<=== Welcome to Employee Wage Computation Program ===>");
+		int FULL_TIME =1;
+		
+		double attendance = Math.floor(Math.random()*10) %2;
+		
+		if (attendance == FULL_TIME)
+			System.out.println("Employee is present!");
+		else
+			System.out.println("Employee is absent!");
 	
 	}
 
