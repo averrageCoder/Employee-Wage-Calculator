0fdea157bb682d643b5dd449d5b3c646e9b110e3
Harsh R Jain <rjainharsh@gmail.com>
2021-08-20 17:03:20 +0530

    ADDED: part time wages

--- src/com/bridgelabz/employeewagecalculator/EmployeeWageCalculator.java
+++ src/com/bridgelabz/employeewagecalculator/EmployeeWageCalculator.java
@@ -4,16 +4,19 @@ public class EmployeeWageCalculator {
 	
 	public static void main(String[] args) {
 
-		int FULL_TIME =1;
+		int PART_TIME=2;
+		int FULL_TIME=1;
 		int EMP_RATE_PER_HOUR = 20;
 		
 		int empWage = 0;
 		int empHrs = 0;
 		
-		double attendance = Math.floor(Math.random()*10) %2;
+		double attendance = Math.floor(Math.random()*10) %3;
 		
 		if (attendance == FULL_TIME)
 			empHrs = 8;
+		else if (attendance == PART_TIME)
+			empHrs = 4;
 		else
 			empHrs = 0;
 		
