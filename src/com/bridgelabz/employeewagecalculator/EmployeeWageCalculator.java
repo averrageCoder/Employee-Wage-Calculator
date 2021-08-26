c0d13e533f504668172fe479859fcf9397b8e85e
Harsh R Jain <rjainharsh@gmail.com>
2021-08-26 17:22:56 +0530

    REFACTORED: Method used to compute employee wage

--- src/com/bridgelabz/employeewagecalculator/EmployeeWageCalculator.java
+++ src/com/bridgelabz/employeewagecalculator/EmployeeWageCalculator.java
@@ -1,61 +1,36 @@
 package com.bridgelabz.employeewagecalculator;
 
 public class EmployeeWageCalculator {
-	
-	private static final int PART_TIME=2;
-	private static final int FULL_TIME=1;
-	private static final int EMP_RATE_PER_HOUR = 20;
-	private static final int NUM_WORKING_DAYS = 20;
-	private static final int MAX_WORK_HRS = 100;
-	
-	private int empHrs;
-	private int total_empHrs;
-	private int total_workingDays;
-	
-	public static void EmployeeWageCalculator() {
-		int empHrs = 0;
-		int total_empHrs = 0;
-		int total_workingDays = 0;
-	}
-	
-	public static int getEmployeeHrs(int attendance) {
-		int empHrs;
-		switch(attendance) {
-		
-		case FULL_TIME:
-			empHrs = 8;
-			break;
-			
-		case PART_TIME:
-			empHrs = 4;
-			break;
-			
-		default:
-			empHrs = 0;
-		
+	public static final int IS_PART_TIME= 1;
+	public static final int IS_FULL_TIME= 2;
+	public static final int EMP_RATE_PER_HOUR = 20;
+	public static final int NUM_OF_WORKING_DAYS = 2;
+	public static final int MAX_HRS_IN_MONTH = 10;
+	public static int computeEmpWage() {
+		// Variables
+		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+		// Computation
+		while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+				totalWorkingDays++;
+				int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+				switch (empCheck) {
+				case IS_PART_TIME:
+					empHrs = 4;
+					break;
+				case IS_FULL_TIME:
+					empHrs = 8;
+					break;
+				default:
+					empHrs = 0;
+				}
+				totalEmpHrs += empHrs;
+				System.out.println("Day#: " + totalWorkingDays + " Emp Hr: " +empHrs);
 		}
-		return empHrs;
+		int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Total Emp Wage: " + totalEmpWage);
+		return totalEmpWage;
 	}
-
-	
 	public static void main(String[] args) {
-		
-		EmployeeWageCalculator employeeObject = new EmployeeWageCalculator(); 
-		
-		while(employeeObject.total_empHrs <= MAX_WORK_HRS && employeeObject.total_workingDays < NUM_WORKING_DAYS) {
-			
-			employeeObject.total_workingDays++;
-			int attendance = (int) Math.floor(Math.random()*10) %3;
-			
-			employeeObject.empHrs = getEmployeeHrs(attendance);
-			
-			employeeObject.total_empHrs += employeeObject.empHrs;
-			System.out.println("Day"+employeeObject.total_workingDays+": Emp Hrs = "+employeeObject.empHrs);
-		}
-		
-		int total_empWage = employeeObject.total_empHrs * EMP_RATE_PER_HOUR;
-		System.out.println("Total Employee Wage : "+total_empWage);
-	
+		computeEmpWage();
 	}
-
 }
