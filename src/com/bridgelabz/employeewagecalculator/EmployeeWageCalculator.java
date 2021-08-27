17d9543905bc7e886f989f404330d2aee0b980dd
Harsh R Jain <rjainharsh@gmail.com>
2021-08-27 22:29:32 +0530

    ADDED: using oops concept to save total wage for each company

--- src/com/bridgelabz/employeewagecalculator/EmployeeWageCalculator.java
+++ src/com/bridgelabz/employeewagecalculator/EmployeeWageCalculator.java
@@ -8,6 +8,7 @@ public class EmployeeWageCalculator {
 	private int empRateperHour;
 	private int numOfWorkingDays;
 	private int maxHrsPerMonth;
+	private int totalEmpWage;
 	
 	public EmployeeWageCalculator(String company, int empRateperHour, int numOfWorkingDays, int maxHrsPerMonth) {
 		
@@ -18,7 +19,7 @@ public class EmployeeWageCalculator {
 		
 	}
 	
-	public int computeEmpWage() {
+	public void computeEmpWage() {
 		// Variables
 		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
 		// Computation
@@ -38,14 +39,20 @@ public class EmployeeWageCalculator {
 				totalEmpHrs += empHrs;
 				System.out.println("Day#: " + totalWorkingDays + " Emp Hr: " +empHrs);
 		}
-		int totalEmpWage = totalEmpHrs * this.empRateperHour;
-		System.out.println("Total Emp Wage for "+this.company+" is " + totalEmpWage);
-		return totalEmpWage;
+		totalEmpWage = totalEmpHrs * this.empRateperHour;
+		//System.out.println("Total Emp Wage for "+this.company+" is " + totalEmpWage);
+		//return totalEmpWage;
+	}
+	@Override
+	public String toString() {
+		return "Total Emp Wage for Company: "+company+" is: "+ totalEmpWage;
 	}
 	public static void main(String[] args) {
 		EmployeeWageCalculator dMart = new EmployeeWageCalculator("DMart", 20, 2, 10);
 		dMart.computeEmpWage();
+		System.out.println(dMart);
 		EmployeeWageCalculator reliance = new EmployeeWageCalculator("Reliance", 10, 2, 10);
 		reliance.computeEmpWage();
+		System.out.println(reliance);
 	}
 }
